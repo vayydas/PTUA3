9d7e84b1b9fa1c89fe53589f27e1ab48ba4fe7e4
Vaidas Urbelis <urbelis@gmail.com>
2023-02-08 20:08:10 +0100

    pirmas

---
+++ for.py
@@ -0,0 +1,25 @@
+for i in range(1, 5):
+    print(i)
+else:
+    print("ciklas baigtas")
+
+for i in range(1, 5):
+    print(2)
+else:
+    print("ciklas baigtas")
+
+for i in range(1, 5, 2):        # 2 duoda reiksme zinksniui, skaicius 5 yra neitraukiamas, 
+    print(i)
+else:
+    print("ciklas baigtas")
+
+
+for i in list(range(1, 5)):
+    print(i)
+else:
+    print("ciklas baigtas")
+
+
+for i in range(1, 5):
+    print(i)
+
