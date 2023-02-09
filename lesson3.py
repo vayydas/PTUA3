128dabefc0e1d0deca6027b36d03518aa8ce7111
Vaidas Urbelis <urbelis@gmail.com>
2023-02-09 13:47:06 +0100

    adding string1.py

---
+++ lesson3.py
@@ -0,0 +1,32 @@
+#my_list = [4, 8, 9, 4, 7, 5, 1.5 ]
+
+#print(sum(my_list))
+
+
+my_list = [4, 8, 9, 4, 7, 5, 1.5 ]
+
+#a = my_list[0] * my_list[2] * my_list[1] * my_list[3] * my_list[4] * my_list[5] * my_list[6]
+
+
+#a = my_list[0: * :-1]          netinka
+
+#print(a)
+
+
+#print(type(a))
+
+
+#print(type(my_list))
+
+#length = len(my_list)
+
+
+#print(len(my_list))
+
+for a in my_list:
+    b = a * 1
+    c = b * a
+print(c)
+
+
+
---
+++ string1.py
@@ -0,0 +1,18 @@
+greeting = "hello, my name is"
+name = "Kristina"
+
+completed_greeting = f"{greeting} {name}"       #apjungimas 
+
+print(completed_greeting)
+
+# arba sekantis varijantas
+
+greeting = "hello, my name is"
+name = "Kristina"
+
+completed_greeting = greeting + " " + name      #apjungimas 
+
+print(completed_greeting)
+
+
+
