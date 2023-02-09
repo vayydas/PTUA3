acc2291ef6c6f263bf7686f357b1de9430a764c5
Vaidas Urbelis <urbelis@gmail.com>
2023-02-09 13:25:10 +0100

    adding expresion.py

---
+++ expresion.py
@@ -0,0 +1,22 @@
+#c = "a" + "b"
+
+#print(c)
+
+#c = 0 / 21
+
+#print(c)
+
+
+lengh = 5
+breadth = 2
+
+area = lengh * breadth
+
+print("plotas lygus", area)
+
+print("perimetras lygus", 2 * (lengh + breadth))
+
+
+
+
+
