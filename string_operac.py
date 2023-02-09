1ca0b44f21e46ddf0f50a77aa65a868037dacac1
Vaidas Urbelis <urbelis@gmail.com>
2023-02-09 14:03:04 +0100

    first commit

---
+++ string_operac.py
@@ -0,0 +1,16 @@
+#name = "Vaidas Urbelis"
+
+#print (name.replace("Vaidas", "das"))
+
+
+age = 5
+sentence = f"{I am (age) year old}"
+print {sentence}
+
+
+
+number1 = int(input("iveskite skaiciu1":    ))
+number2 = int(input("iveskite skaiciu2":    ))
+number3 = int(input("iveskite skaiciu3":    ))
+
+my_list = 
---
+++ typ_convertavimas.py
@@ -0,0 +1,29 @@
+#a = "55"
+#b = int(a)
+#c = float(a)
+
+#print(b + c)
+
+#name = input("enter you name:  ")
+#age = input("enter your age:  ")
+#print(f"your name is {name} , your are {age} years old")
+
+
+#vorname = input("iveskite savo varda:  ")
+#name = input("iveskite savo pavarde:  ")
+#jahre = int(input("iveskite savo amziu:  "))
+#geboren = (2023 - (jahre))
+#print(vorname    + name, ("jusu gimimo metai yra:"), geboren)
+
+
+
+#sentence = input("iveskite sakini:  ")
+#print(sentence[::-1])
+#print(sentence[::2])
+
+#number1 = float(input("iveskite skaiciu:   "))
+#number2 = float(input("iveskite skaiciu:   "))
+#number3 = number1 * number2
+#print(number3)
+
+
