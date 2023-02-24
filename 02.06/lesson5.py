25044be7a2d89154c9f1e925194727fe1164fb34
vayydas <124807016+vayydas@users.noreply.github.com>
2023-02-24 19:04:59 +0100

    Adding lesson 15 (#1)
    * adding new file
    
    * 2 test
    
    * adding 02.06

---
+++ 02.06/lesson5.py
@@ -0,0 +1,53 @@
+#countrys_and_capitals = {
+#    "Litauen": "Vilnius",
+   # "Poland": "Warsaw",
+  #  "Letland": {
+  #      "capital": "Riga",
+  #      "population": 2000000,
+  #  }
+#}
+#print(countrys_and_capitals["Letland"]["population"])
+
+
+my_dict = {
+    "Autoturgus": {
+        "Mercedes":{
+            "dyzelis",
+            "rida": 50000,
+            "kaina": 25000,
+        }
+        "BMW": {
+            "Benzinas",
+            "rida": 45000,
+            "kaina": 35000,
+        }
+        "Tesla": {
+            "elektra",
+            "rida": 5000,
+            "kaina": 70000,
+        }
+        "Hundai":{
+            "Hibridas",
+            "rida": 1000,
+            "kaina": 50000,
+        }}}
+print(my_dict["Mercedes"]["rida"])
+
+
+
+
+my_dict = {
+    "Autoturgus":{
+        "BMW":{
+            "rida": 50000,
+            "Kuras": "dyzelis",
+            "Kaina": 35000,
+        }
+        "Mercedes":{
+            "rida": 65000,
+            "Kuras": "benzinas",
+            "Kaina": 37000,
+        }
+    }
+}
+
---
+++ if.py
@@ -0,0 +1,62 @@
+# number = 23
+# guess = int(input("iveskite sveika skaiciu:"  ))
+
+# if guess == number:
+#     print("sveikiname, jus atspejote, ")  #naujo bloko pradzia
+
+#     print("(nors nieko ir nelaimejote)",)   #bloko pabaiga
+
+
+# elif guess < number :
+#     print("neteisingai, uzduotas skaiciuc didesni.")    #dar vienas blokas
+#     #bloko viduje jus galite vykdyti viska...
+# else:
+#     print("uzduotas skaicius yra mazesnis.")
+#     #kad papulti cia , guess turi buti didesnis nei number
+
+# print("baigta")
+
+
+
+
+
+# x = 5
+# if x < 5:
+#     y = -1
+#     z = 12
+# elif x > 5:
+#     y = -2
+#     z = 21
+# else:
+#     y = 8
+#     z = 0
+# print(x, y, z)      # 5 8 0 
+
+
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")      # a is greater than b
+
+ # a is greater than b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
